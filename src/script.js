4fed6768254962052957e2c36f9eaee27c46d639
piyeldev <frankthecrafter23@gmail.com>
2024-02-17 13:34:20 +0800

    finished bmi-calculator 🚀

--- src/script.js
+++ src/script.js
@@ -4,14 +4,11 @@ var weightVal = document.getElementById("weight");
 var heightVal = document.getElementById("height");
 var unitSelectorHeight = document.getElementById("height-unit")
 var unitSelectorWeight = document.getElementById("weight-unit")
+var resultElement = document.getElementById("result")
+var resultUnitElement = document.getElementById("result-unit")
+var arrow = document.getElementById("arrow")
 
-var text = document.getElementById("underweight-p")
-const bent = new CircleType(text)
-bent.radius(200)
 
-function print(stringArg) {
-  console.log(stringArg)
-}
 convertBtn.addEventListener("click", function () {
   var weight = weightVal.value;
   var height = heightVal.value;
@@ -43,9 +40,11 @@ function calculateBmi(weight, height) {
     `Height: ${height} Weight: ${weight}`
   );
   var answer = (weight / (height**2)).toFixed(2);
-
+  resultElement.textContent = answer + ' kg/m²'
   mathjaxFormula.textContent = `\\[BMI = {weight (kg) \\over height (m)^2}= {${weight}kg\\over${height}m^2} = ${answer}\\]`;
   MathJax.typeset();
+
+  rotateArrow(answer)
 }
 
 function removeErrors() {
@@ -63,8 +62,6 @@ function converter(weight, height) {
 
   switch (weightUnit) {
     case "Kilograms (kg)":
-      // just return kg
-      print("kilos")
       convertedWeight = weight
       break;
     case "Pounds (lbs)":
@@ -96,3 +93,28 @@ function converter(weight, height) {
   return values
 }
 
+function rotateArrow(answer) {
+  if (answer < 13) {
+    arrow.style.transform = "rotate(0deg)"
+  } else if (answer > 30) {
+    arrow.style.transform = "rotate(180deg)"
+  }
+  else {
+    var interpolation = linearInterpolation(answer)
+    arrow.style.transform = "rotate("+interpolation+"deg)"
+  }
+  
+}
+
+
+function linearInterpolation(x) {
+  // based on the formula x=y1+(x-x1)*((y2-y1)/(x2-x1))
+  const x1 = 13
+  const x2 = 30
+  const y1 = 0
+  const y2 = 180
+  var divisionPart = (y2-y1)/(x2-x1)
+  var additionPart = y1 + (x - x1)
+  y = additionPart * divisionPart
+  return y
+} 
